000a6c9417daa21caf26f51f269b2124981cc50d
Antravaya <120424177+Antravaya@users.noreply.github.com>
2023-05-19 10:00:24 -0400

    Add files via upload

---
+++ Temperature/Temperature.py
@@ -0,0 +1,8 @@
+def main():
+    
+    Farhrenheit = eval(input("Enter a Fahrenheit temperature:"))
+    celsius = (Farhrenheit - 32) * 5/9
+    
+    print("The Celsius temperature is:", celsius)
+
+main()
